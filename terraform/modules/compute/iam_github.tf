c651eb1dd7f51f8de49ec06dd9bf58287a057314
Viresh Solanki <vireshsolanki1027@gmail.com>
2026-03-06 21:41:04 +0530

    feat: enable terraform-managed OIDC provider

--- terraform/modules/compute/iam_github.tf
+++ terraform/modules/compute/iam_github.tf
@@ -1,12 +1,9 @@
-
-/*
-# OIDC Provider for GitHub Actions (managed manually via CLI)
+# OIDC Provider for GitHub Actions
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"] # Current GitHub Actions GPG thumbprints
 }
-*/
 
 # IAM Role for GitHub Actions
 resource "aws_iam_role" "github_actions" {
@@ -19,7 +16,7 @@ resource "aws_iam_role" "github_actions" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::833899002429:oidc-provider/token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
         Condition = {
           StringLike = {
